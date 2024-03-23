f0a008be62f9314309297eae458f76dcbaa1b43a
Timothy Ndiritu <timmyndiritu@gmail.com>
2024-03-23 21:42:02 +0300

    queen piece complete

---
+++ src/com/chess/engine/board/Board.java
@@ -0,0 +1,10 @@
+ 
+package com.chess.engine.board;
+
+
+ 
+public class Board {
+    public chessTile getTile(final int tileCoordinate){
+        return null;   
+    }
+}
---
+++ src/com/chess/engine/board/chessTile.java
@@ -0,0 +1,73 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class chessTile {
+
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static chessTile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+
+    private chessTile(final int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends chessTile {
+
+        private EmptyTile(final int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends chessTile {
+
+        private final Piece pieceOnTile;
+
+        private OccupiedTile(int tileCoordinate,final Piece pieceOnTile) {
+            super(tileCoordinate);
+
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
